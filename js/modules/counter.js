1b948118d14c16bbd342fcaded215b649ae6cdda
michaelscimeca <mikey@rsq.com>
2019-05-20 15:49:22 -0500

    Spell checked js and html

--- js/modules/counter.js
+++ js/modules/counter.js
@@ -1,44 +1,113 @@
 'use strict';
 module.exports = function () {
-  const list = document.querySelectorAll('form input');
-  const showTotalScore = document.querySelector('#totalScore span');
-  const Range = document.querySelector('#range span');
-  let form = {};
-  let total = 0;
+  const strategicList = document.querySelectorAll('#strategic input');
+  const strategicScore = document.querySelector('#strategic-totalScore span');
+  const strategicRange = document.querySelector('#strategic-range span');
+  let strategicListForm = {};
+  let strategicTotal = 0;
+  const complexityList = document.querySelectorAll('#complexity input');
+  const complexityScore = document.querySelector('#complexity-totalScore span');
+  const complexityRange = document.querySelector('#complexity-range span');
 
-  function addValues () {
-    total = 0;
-    for (let val in form) {
-      if (form.hasOwnProperty(val)) {
-        total += form[val];
+  const tier = document.querySelector('#tier span');
+  let complexityListForm = {};
+  let complexityTotal = 0;
+  let totalAmount = 0;
+
+  const total = document.querySelector('#total span');
+
+  function strategicAddValues () {
+    strategicTotal = 0;
+    for (let val in strategicListForm) {
+      if (strategicListForm.hasOwnProperty(val)) {
+        strategicTotal += strategicListForm[val];
       }
     }
-    if (total !== 0) {
-      showTotalScore.innerHTML = total;
+    if (strategicTotal !== 0) {
+      strategicScore.innerHTML = strategicTotal;
     } else {
-      showTotalScore.innerHTML = '';
+      strategicScore.innerHTML = '';
     }
-    if (total === 0) {
-      Range.innerHTML = '';
-    } else if (total > 0 && total <= 5) {
-      Range.innerHTML = 'Low';
-    } else if (total >= 6 && total <= 9) {
-      Range.innerHTML = 'Medium';
+    if (strategicTotal === 0) {
+      strategicRange.innerHTML = '';
+    } else if (strategicTotal > 0 && strategicTotal <= 5) {
+      strategicRange.innerHTML = 'Low';
+    } else if (strategicTotal >= 6 && strategicTotal <= 9) {
+      strategicRange.innerHTML = 'Medium';
     } else {
-      Range.innerHTML = 'High';
+      strategicRange.innerHTML = 'High';
     }
-  }
 
+    if ((strategicTotal + complexityTotal) > 19) {
+      tier.innerHTML = 1;
+    } else if ((strategicTotal + complexityTotal) > 15) {
+      tier.innerHTML = 2;
+    } else if ((strategicTotal + complexityTotal) > 8) {
+      tier.innerHTML = 3;
+    } else if ((strategicTotal + complexityTotal) > 0) {
+      tier.innerHTML = 4;
+    }
+    totalAmount = strategicTotal + complexityTotal;
+    total.innerHTML = totalAmount;
+  }
   function onClick (e) {
     if (e.target.checked) {
-      form[e.target.name] = parseInt(e.target.value);
+      strategicListForm[e.target.name] = parseInt(e.target.value);
     } else {
-      form[e.target.name] = 0;
+      strategicListForm[e.target.name] = 0;
     }
-    addValues();
+    strategicAddValues();
   }
-  for (let i = 0; i < list.length; i++) {
-    form[list[i].name] = 0;
-    list[i].addEventListener('click', onClick);
+  for (let i = 0; i < strategicList.length; i++) {
+    strategicListForm[strategicList[i].name] = 0;
+    strategicList[i].addEventListener('click', onClick);
+  }
+
+  function complexityAddValues () {
+    complexityTotal = 0;
+    for (let val in complexityListForm) {
+      if (complexityListForm.hasOwnProperty(val)) {
+        complexityTotal += complexityListForm[val];
+      }
+    }
+    if (complexityTotal !== 0) {
+      complexityScore.innerHTML = complexityTotal;
+    } else {
+      complexityScore.innerHTML = '';
+    }
+    if (complexityTotal === 0) {
+      complexityRange.innerHTML = '';
+    } else if (complexityTotal > 0 && complexityTotal <= 5) {
+      complexityRange.innerHTML = 'Low';
+    } else if (complexityTotal >= 6 && complexityTotal <= 9) {
+      complexityRange.innerHTML = 'Medium';
+    } else {
+      complexityRange.innerHTML = 'High';
+    }
+
+    if ((strategicTotal + complexityTotal) > 19) {
+      tier.innerHTML = 1;
+    } else if ((strategicTotal + complexityTotal) > 15) {
+      tier.innerHTML = 2;
+    } else if ((strategicTotal + complexityTotal) > 8) {
+      tier.innerHTML = 3;
+    } else if ((strategicTotal + complexityTotal) > 0) {
+      tier.innerHTML = 4;
+    }
+    totalAmount = strategicTotal + complexityTotal;
+    total.innerHTML = totalAmount;
+  }
+  function onClickComplexity (e) {
+    if (e.target.checked) {
+      complexityListForm[e.target.name] = parseInt(e.target.value);
+    } else {
+      complexityListForm[e.target.name] = 0;
+    }
+
+    complexityAddValues();
+  }
+  for (let i = 0; i < complexityList.length; i++) {
+    complexityListForm[complexityList[i].name] = 0;
+    complexityList[i].addEventListener('click', onClickComplexity);
   }
 };
